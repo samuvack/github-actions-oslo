df664faeb8ff59fb18c6a5280e6410b105003b3b
samuvack <samuel.vanackere@vlaanderen.be>
2023-04-27 12:18:45 +0200

    fix overview

--- scripts/oslo-applicatieprofiel-controle.py
+++ scripts/oslo-applicatieprofiel-controle.py
@@ -10,6 +10,8 @@ outputfile = '../README.md'
 
 
 from selenium import webdriver
+
+"""
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
@@ -23,6 +25,8 @@ chromedriver_autoinstaller.install()
 # and if it doesn't exist, download it automatically,
 # then add chromedriver to path
 
+
+"""
 chrome_options = webdriver.ChromeOptions()
 # Add your options as needed
 options = [
@@ -39,6 +43,7 @@ options = [
     # "--disable-dev-shm-usage",
     # '--remote-debugging-port=9222'
 ]
+
 
 for option in options:
     chrome_options.add_argument(option)
@@ -197,29 +202,33 @@ def getData(link, driver):
     return stanDict
 
 # analyseer het AP
+global geenfixme
 geenfixme = 0
-geenbeschrijving = 0
-geentype = 0
+global geenbeschrijving
+geenbeschrijving  = 0
+global geentype
+geentype  = 0
+global geenkardinaliteit
 geenkardinaliteit = 0
-geendefenitie = 0
-text = ""
+global geendefinitie
+geendefinitie = 0
 
 
-def analyse(data, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefenitie):
+def analyse(data, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefinitie):
     resultaat = ""
     for soort in data:
         for entiteit in data[soort]:
 
             # link van klasse zelf checken
             if "fixme" in list(entiteit.values())[0][0]['link']:
-                geenfixme = geenfixme + 1
-                resultaat += "```diff \ - fixme gevonden in de link van klasse \"{}\" ``` <br>".format(
+                geenfixme += 1
+                resultaat += "fixme gevonden in de link van klasse \"{}\"  \ ".format(
                     list(entiteit.keys())[0])
                 resultaat += "\n"
 
             # beschrijving van de klasse zelf checken
             if list(entiteit.values())[0][1]['beschrijving'] == "":
-                resultaat += "```diff \ - Geen beschrijving gevonden bij klasse \"{}\" ``` <br>".format(
+                resultaat += "Geen beschrijving gevonden bij klasse \"{}\"  \ ".format(
                     list(entiteit.keys())[0])
                 resultaat += "\n"
 
@@ -230,83 +239,98 @@ def analyse(data, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geen
 
             cel = attributen[0]
             if "fixme" in cel[list(cel.keys())[0]]:
-                geenfixme = geenfixme + 1
-                resultaat += "```diff \ - fixme gevonden in attribuut \"{}\" van klasse \"{}\" ``` <br>".format(
+                geenfixme += 1
+
+                resultaat += "fixme gevonden in attribuut \"{}\" van klasse \"{}\"  \ ".format(
                     attribuut, list(entiteit.keys())[0])
                 resultaat += "\n"
             cel = attributen[1]
             try:
                 if "fixme" in cel[list(cel.keys())[0]]:
-                    geenfixme = geenfixme + 1
-                    resultaat += "```diff \ - fixme gevonden in attribuut \"{}\" van klasse \"{}\"``` <br>".format(
+                    geenfixme += 1
+    
+                    resultaat += "fixme gevonden in attribuut \"{}\" van klasse \"{}\" \ ".format(
                         attribuut, list(entiteit.keys())[0])
                     resultaat += "\n"
             except:
                 geentype = geentype + 1
-                resultaat += "```diff \ - Geen verwacht type gevonden bij attribuut \"{}\" van klasse \"{}\" ``` <br>".format(
+                resultaat += "Geen verwacht type gevonden bij attribuut \"{}\" van klasse \"{}\"  \ ".format(
                     attribuut, list(entiteit.keys())[0])
                 resultaat += "\n"
             cel = attributen[2]
             if cel == "":
                 geenkardinaliteit = geenkardinaliteit + 1
-                resultaat += "```diff \ - Kardinaliteit ontbreekt in attribuut \"{}\" van klasse \"{}\"``` <br>".format(
+                resultaat += "Kardinaliteit ontbreekt in attribuut \"{}\" van klasse \"{}\" \ ".format(
                     attribuut, list(entiteit.keys())[0])
                 resultaat += "\n"
             cel = attributen[3]
             if cel == "":
-                geendefenitie = geendefenitie + 1
-                resultaat += "```diff \ - Definitie ontbreekt in attribuut \"{}\" van klasse \"{}\"``` <br>".format(
+                geendefinitie = geendefinitie + 1
+                
+                resultaat += "Definitie ontbreekt in attribuut \"{}\" van klasse \"{}\" \ ".format(
                     attribuut, list(entiteit.keys())[0])
                 resultaat += "\n"
 
     if resultaat == "":
-        resultaat += "```diff \ + Alles is in orde!```"
+        resultaat += "Alles is in orde!"
 
-    return resultaat, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefenitie
+    return resultaat, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefinitie
 
 
 driver = webdriver.Chrome()
 
 links = ['https://data.vlaanderen.be/doc/applicatieprofiel/bedrijventerrein/', 'https://data.vlaanderen.be/doc/applicatieprofiel/besluit-publicatie/', 'https://data.vlaanderen.be/doc/applicatieprofiel/besluitvorming', 'https://data.vlaanderen.be/doc/applicatieprofiel/contactvoorkeuren/', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-event', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-object', 'https://data.vlaanderen.be/doc/applicatieprofiel/DCAT-AP-VL/erkendestandaard/2021-12-02', 'https://data.vlaanderen.be/doc/applicatieprofiel/DCAT-AP-VL', 'https://data.vlaanderen.be/doc/applicatieprofiel/dienst-transactiemodel/', 'https://data.vlaanderen.be/doc/applicatieprofiel/dossier/', 'https://data.vlaanderen.be/doc/applicatieprofiel/generieke-terugmeldfaciliteit/', 'https://data.vlaanderen.be/doc/applicatieprofiel/issue-tracking-voor-burgers-en-organisaties/', 'https://data.vlaanderen.be/doc/applicatieprofiel/ldes', 'https://data.vlaanderen.be/doc/applicatieprofiel/mandatendatabank/', 'https://data.vlaanderen.be/doc/applicatieprofiel/metadata-dcat/', 'https://data.vlaanderen.be/doc/applicatieprofiel/mobiliteit/dienstregeling-en-planning/stopplaatsen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/mobiliteit/dienstregeling-en-planning/tijdstabellen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/mobiliteit/dienstregeling-en-planning/voertuigplanning/', 'https://data.vlaanderen.be/doc/applicatieprofiel/mobiliteit-trips-en-aanbod', 'https://data.vlaanderen.be/doc/applicatieprofiel/notificatie-basis/', 'https://data.vlaanderen.be/doc/applicatieprofiel/openbaar-domein/', 'https://data.vlaanderen.be/doc/applicatieprofiel/perceel/', 'https://data.vlaanderen.be/doc/applicatieprofiel/logies-basis/', 'https://data.vlaanderen.be/doc/applicatieprofiel/mobiliteit/vervoersknooppunten', 'https://data.vlaanderen.be/doc/applicatieprofiel/vlaamse-codex/', 'https://data.vlaanderen.be/doc/applicatieprofiel/begraafplaatsenbeheer/', 'https://data.vlaanderen.be/doc/applicatieprofiel/begroeid-voorkomen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/infrastructuurelementen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/onbegroeid-voorkomen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/terreindelen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/vegetatie-elementen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/watervoorkomen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/waterdelen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/begraafplaatsen', 'https://data.vlaanderen.be/doc/applicatieprofiel/begraafplaatsenbeheer', 'https://data.vlaanderen.be/doc/applicatieprofiel/begroeid-voorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/gebieden', 'https://data.vlaanderen.be/doc/applicatieprofiel/infrastructuurelementen', 'https://data.vlaanderen.be/doc/applicatieprofiel/onbegroeid-voorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/terreindelen',
          'https://data.vlaanderen.be/doc/applicatieprofiel/vegetatie-elementen', 'https://data.vlaanderen.be/doc/applicatieprofiel/waterdelen', 'https://data.vlaanderen.be/doc/applicatieprofiel/watervoorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/openbaar-domein', 'https://data.vlaanderen.be/doc/applicatieprofiel/openbaar-domein', 'https://data.vlaanderen.be/doc/applicatieprofiel/gebieden', 'https://data.vlaanderen.be/doc/applicatieprofiel/infrastructuurelementen', 'https://data.vlaanderen.be/doc/applicatieprofiel/vegetatie-elementen', 'https://data.vlaanderen.be/doc/applicatieprofiel/terreindelen', 'https://data.vlaanderen.be/doc/applicatieprofiel/begroeid-voorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/onbegroeid-voorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/waterdelen', 'https://data.vlaanderen.be/doc/applicatieprofiel/watervoorkomen', 'https://data.vlaanderen.be/doc/applicatieprofiel/begraafplaatsen', 'https://data.vlaanderen.be/doc/applicatieprofiel/begraafplaatsenbeheer', 'https://data.vlaanderen.be/doc/applicatieprofiel/bedrijventerrein/', 'https://data.vlaanderen.be/doc/applicatieprofiel/dossier/', 'https://data.vlaanderen.be/doc/applicatieprofiel/adresregister/', 'https://data.vlaanderen.be/doc/applicatieprofiel/besluit-mobiliteit/', 'https://data.vlaanderen.be/doc/applicatieprofiel/subsidieregister/', 'https://data.vlaanderen.be/doc/applicatieprofiel/bestuurlijk-sanctieregister', 'https://data.vlaanderen.be/doc/applicatieprofiel/bodem-en-ondergrond/bodem-en-ondergrond/kandidaatstandaard/2022-04-28', 'https://data.vlaanderen.be/doc/applicatieprofiel/dienstencataloog/', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultuurparticipatie', 'https://data.vlaanderen.be/doc/applicatieprofiel/FeitelijkeVerenigingen/', 'https://data.vlaanderen.be/doc/applicatieprofiel/financiele-data', 'https://test.data.vlaanderen.be/doc/applicatieprofiel/gebouwenregister/', 'https://data.vlaanderen.be/doc/applicatieprofiel/inname-openbaar-domein/', 'https://data.vlaanderen.be/doc/applicatieprofiel/loongegevens', 'https://data.vlaanderen.be/doc/applicatieprofiel/observaties-en-metingen/kandidaatstandaard/2022-04-28', 'https://data.vlaanderen.be/doc/applicatieprofiel/organisatie-basis/', 'https://data.vlaanderen.be/doc/applicatieprofiel/persoon-basis/', 'https://data.vlaanderen.be/doc/applicatieprofiel/sensoren-en-bemonstering/kandidaatstandaard/2022-04-28', 'https://data.vlaanderen.be/doc/applicatieprofiel/verkeersborden/', 'https://data.vlaanderen.be/doc/applicatieprofiel/wegenregister/', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-object', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-event', 'https://data.vlaanderen.be/doc/applicatieprofiel/cultuur-en-jeugdinfrastructuur', 'https://data.vlaanderen.be/doc/applicatieprofiel/waterkwaliteit', 'https://data.vlaanderen.be/doc/applicatieprofiel/statistiek', 'https://data.vlaanderen.be/doc/applicatieprofiel/datakwaliteit']
-create_empty_file("README.md")
+create_empty_file(outputfile)
 write_to_file(outputfile, "## OSLO applicatie profielen HTML validatie")
 write_to_file(outputfile, '\n')
-write_to_file(outputfile, '\')
+write_to_file(outputfile, "\\")
 
 
 # using now() to get current time
-current_time = datetime.now() + timedelta(hours=1)
+current_time = datetime.now()
 
 # Printing value of now.
 print(current_time)
 
 write_to_file(
     '../README.md', """```diff
-    ! Dit document is automatisch gevalideerd op : """ + str(current_time) + """```""")
+    ! Dit document is automatisch gegenereerd op : """ + str(current_time) + """```""")
 write_to_file(outputfile, '\n')
+
+text = ''
 
 for link in links:
     data = getData(link, driver)
-    resultaat = analyse(data, geenfixme, geenbeschrijving,
-                        geentype, geenkardinaliteit, geendefenitie)[0]
+    resultaat, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefinitie = analyse(data, geenfixme, geenbeschrijving, geentype, geenkardinaliteit, geendefinitie)
     if (resultaat == """Alles is in orde!"""):
-        write_to_file(outputfile, '\')
+        """
         write_to_file(outputfile, '\n')
         write_to_file(outputfile, "["+link+"]("+link+")")
-        write_to_file(outputfile, '\')
+        write_to_file(outputfile, '\\')
         write_to_file(outputfile, '\n')
         print(resultaat)
         write_to_file(outputfile, resultaat)
-        
-        write_to_file(outputfile, '\')
+        write_to_file(outputfile, '\\')
+        """
     else:
-        print(link, "\")
-        print(resultaat, "\\")
-        write_to_file(outputfile, '\')
-        write_to_file(outputfile, "["+link+"]("+link+")")
-        write_to_file(outputfile, '\')
-        print(resultaat)
-        write_to_file(outputfile, resultaat)
-        write_to_file(outputfile, '\')
+        
+        text +='\n'
+        text += "["+link+"]("+link+")"
+        text += '\\'
+        text += '\n'
+        text += resultaat
+        text += '\\'
+        
     
+overview = """
+| Fixme gevonden in attribuut               | """ + str(geenfixme)  + """  |
+| Geen verwacht type gevonden bij attribuut | """ + str(geentype)   + """  |
+| Kardinaliteit ontbreekt in attribuut      | """ + str(geenkardinaliteit)  + """  |
+| Definitie ontbreekt in attribuut          | """ + str(geendefinitie) + """  |
+| Geen beschrijving gevonden bij klasse     | """ + str(geenbeschrijving) + """  |
+"""
+
+
+write_to_file(outputfile, overview)
+write_to_file(outputfile, text)
