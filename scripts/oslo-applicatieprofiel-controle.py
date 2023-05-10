d24513ca62349bba0ec71f366ff55bff3add4831
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2023-05-10 12:33:28 +0200

    Update oslo-applicatieprofiel-controle.py

--- scripts/oslo-applicatieprofiel-controle.py
+++ scripts/oslo-applicatieprofiel-controle.py
@@ -62,7 +62,7 @@ def create_empty_file(filename):
         None
     """
     try:
-        with open(filename, 'r+', errors='replace') as file:
+        with open(filename, 'r+',encoding='utf-8') as file:
             file.truncate(0)
         print(f'Success: Empty file "{filename}" created.')
     except IOError as e:
@@ -80,7 +80,7 @@ def write_to_file(filename, parameter):
         None
     """
     try:
-        with open(filename, 'a', errors='replace') as output:
+        with open(filename, 'a', encoding='utf-8') as output:
             output.write(parameter)
         print(
             f'Success: Parameter "{parameter}" written to file "{filename}".')
